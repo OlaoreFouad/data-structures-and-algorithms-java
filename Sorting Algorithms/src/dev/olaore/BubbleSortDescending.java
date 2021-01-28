e3675456ded66b30166669e88ef08ba90e1976b2
Foodie <olaorefouad565@gmail.com>
2021-01-28 11:26:30 +0400

    tried out the bubble sort algorithm but in descending order sorting from the right to left. bubbling large numbers to the start of the array and smaller numbers to the end of the array. this was done by setting the last unsorted index to the start of the array, and begin swapping from the end of the array

---
+++ Sorting Algorithms/src/dev/olaore/BubbleSortDescending.java
@@ -0,0 +1,34 @@
+package dev.olaore;
+
+public class BubbleSortDescending {
+
+    public static void main(String[] args) {
+
+        int[] numbers = { 7, -9, 10, 23, -35, 55, 100, -1, 2 };
+
+        for (int lastUnsortedIndex = 0; lastUnsortedIndex >= 0; lastUnsortedIndex++) {
+            for (int index = numbers.length - 1; index > lastUnsortedIndex; index--) {
+                if (numbers[index] > numbers[index - 1]) {
+                    swap(numbers, index, index - 1);
+                }
+            }
+        }
+
+        for (int number: numbers) {
+            System.out.print(number + " ");
+        }
+
+    }
+
+    private static void swap(int[] numbers, int i, int j) {
+        if (numbers[i] == numbers[j]) {
+            return;
+        }
+
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+
+    }
+
+}
