e8d8c140de138096f6ab31637ebd1cbea68149b3
Olaore Fouad <olaorefouad565@gmail.com>
2021-03-19 01:29:54 +0400

    after a long while, was able to work on the merge sort algorithm with the help of a couple refreshers, would need to review as soon as possible - but glad i was able to do it

---
+++ Sorting Algorithms/src/dev/olaore/merge_sort/MergeSort.java
@@ -0,0 +1,52 @@
+package dev.olaore.merge_sort;
+
+import dev.olaore.utils.Utils;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        int[] numbers = Utils.numbers;
+
+        performSplit(numbers, 0, numbers.length);
+
+        for (int number: numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    private static void performSplit(int[] input, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        performSplit(input, start, mid);
+        performSplit(input, mid, end);
+        merge(input, start, mid, end);
+
+    }
+
+    private static void merge(int[] input, int start, int mid, int end) {
+
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+
+        while(i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, (start + tempIndex), mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+    }
+
+}
